31c57bc9192b012ebda420d39d35a7eac74af8cb
Hombre-x <gabisanpar@gmail.com>
2025-08-18 21:31:41 -0500

    🏷️ Add CQRS types

---
+++ src/main/scala/domain/commands/CommandTypes.scala
@@ -0,0 +1,6 @@
+package com.mycode
+package domain.commands
+
+import java.util.UUID
+
+type CommandId = UUID
---
+++ src/main/scala/domain/queries/QueryTypes.scala
@@ -0,0 +1,6 @@
+package com.mycode
+package domain.queries
+
+import java.util.UUID
+
+type QueryId = UUID
