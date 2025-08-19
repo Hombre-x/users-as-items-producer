0ec58603afd95bd7bdbb56012af291a7fa725fb6
Hombre-x <gabisanpar@gmail.com>
2025-08-18 21:21:49 -0500

    🚧 Changed program entrypoint

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,29 +4,30 @@ import cats.effect.{IO, IOApp}
 import cats.effect.std.UUIDGen
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import fs2.kafka.KafkaProducer
-import amenities.MkKafkaProducerSettings
+import fs2.kafka.{KafkaConsumer, KafkaProducer}
+import amenities.{AppResources, MkHttpServer, MkKafkaConsumerSettings, MkKafkaProducerSettings}
+
+import scala.concurrent.duration.*
 import config.Config
+
+import com.comcast.ip4s.Port
+import com.mycode.http.HttpApi
 
 object Main extends IOApp.Simple:
 
   given logger: Logger[IO] = Slf4jLogger.getLoggerFromName("Main")
-  
+
   override def run: IO[Unit] =
-    
+
     Config.load[IO].flatMap: config =>
-        logger.info(s"Loading config for application: $config") >>
-          MkKafkaProducerSettings .make[IO](config.producerConfig).create.use: producerSettings =>
-            
-              KafkaProducer
-                .stream(producerSettings)
-                .evalMap(producer => UUIDGen.randomUUID[IO].map(uuid => (uuid, producer)))
-                .evalMap:
-                  case (uuid, producer) =>
-                    producer.produceOne("warehouse-topic", uuid, "Hi there Mr. Kafka! 🍎")
-                .compile
-                .drain
+      logger.info(s"Loading config for application: $config") >>
+        AppResources.make(config).use: _ =>
+          val httpApi = HttpApi.make[IO]
+          val server  = MkHttpServer.make[IO].ember(config.httpConfig.port, httpApi.routes)
+
+          server.useForever
+            .onCancel(logger.info("Closing server..."))
+
   end run
-  
 
 end Main
