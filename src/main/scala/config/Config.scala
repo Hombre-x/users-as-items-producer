56dddec8d7d96cb7955ef7dcd54933195e668341
Hombre-x <gabisanpar@gmail.com>
2025-08-18 21:20:13 -0500

    🔧 Add port and Kafka environment variables

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -1,19 +1,21 @@
 package com.mycode
 package config
 
+import cats.syntax.all.*
 import cats.effect.Async
-import domain.config.{AppConfig, ConsumerConfig, ProducerConfig}
+import domain.config.{AppConfig, ConsumerConfig, HttpConfig, ProducerConfig}
 
 import ciris.*
+import com.comcast.ip4s.Port
 
 object Config:
 
   def load[F[_]: Async]: F[AppConfig] =
     (
-      env("KAFKA_BOOTSTRAP_SERVER")
-        .default("localhost:9092")
-      )
-      .map(kafkaUri =>
+      env("KAFKA_BOOTSTRAP_SERVER").default("localhost:19092"),
+      env("WAREHOUSE_SERVER_PORT").as[Int].default(9000),
+    )
+      .parMapN( (kafkaUri, port) =>
         AppConfig(
           producerConfig = ProducerConfig(
             kafkaUri
@@ -21,6 +23,10 @@ object Config:
           consumerConfig = ConsumerConfig(
             kafkaUri,
             "default-group"
+          ),
+          httpConfig = HttpConfig(
+              "localhost",
+              Port.fromInt(port).get
           )
         )
       )
--- src/main/scala/domain/config/ConfigTypes.scala
+++ src/main/scala/domain/config/ConfigTypes.scala
@@ -1,10 +1,14 @@
 package com.mycode
 package domain.config
 
+import com.comcast.ip4s.Port
+
 case class AppConfig(
     producerConfig: ProducerConfig,
-    consumerConfig: ConsumerConfig
+    consumerConfig: ConsumerConfig,
+    httpConfig: HttpConfig
 )
 
 case class ProducerConfig(bootstrapServer: String)
 case class ConsumerConfig(bootstrapServer: String, groupId: String)
+case class HttpConfig(host: String, port: Port)
