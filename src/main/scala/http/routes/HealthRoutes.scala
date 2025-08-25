9c38e3117a6efcf45841dc8d294fe282a186a67d
Hombre-x <gabisanpar@gmail.com>
2025-08-25 01:15:07 -0500

    🩺 Add SSE streaming to API

--- src/main/scala/http/routes/HealthRoutes.scala
+++ src/main/scala/http/routes/HealthRoutes.scala
@@ -1,23 +1,31 @@
 package com.mycode
 package http.routes
 
-import cats.Monad
+import cats.effect.Temporal
+import cats.syntax.option.*
+
 import org.http4s.*
 import org.http4s.server.Router
 import org.http4s.dsl.*
+import org.http4s.circe.CirceEntityCodec.*
 
-final class HealthRoutes[F[_] : Monad] extends Http4sDsl[F]:
-  
+import fs2.Stream
+
+import scala.concurrent.duration.*
+
+final class HealthRoutes[F[_]: Temporal] extends Http4sDsl[F]:
+
   private val prefixPath = "/"
-  
+
   private def httpRoutes: HttpRoutes[F] = HttpRoutes.of[F]:
-    case GET -> Root / "health" => Ok("Safe and sound!")
-  
+    case GET -> Root / "health"   => Ok("Safe and sound!")
+    case GET -> Root / "infinite" =>
+      Ok(
+        Stream
+          .awakeEvery[F](1.second)
+          .map(n => ServerSentEvent(data = s"Created $n".some, eventType = "time".some))
+      )
 
   val routes: HttpRoutes[F] = Router(prefixPath -> httpRoutes)
-  
-end HealthRoutes
 
-  
-  
-  
+end HealthRoutes
