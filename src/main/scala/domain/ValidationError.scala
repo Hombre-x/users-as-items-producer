f35fa44ccada602c69236766c84744c153e7a2c7
Hombre-x <gabisanpar@gmail.com>
2025-08-25 01:12:16 -0500

    🚚 Moved validation error to domain.validation

--- src/main/scala/domain/ValidationError.scala
+++
@@ -1,7 +0,0 @@
-package com.mycode
-package domain
-
-import cats.data.NonEmptyList
-import io.circe.Codec
-
-case class ValidationError(message: String, errors: NonEmptyList[String]) derives Codec
---
+++ src/main/scala/domain/validation/ValidationError.scala
@@ -0,0 +1,8 @@
+package com.mycode
+package domain.validation
+
+import scala.util.control.NoStackTrace
+import cats.data.NonEmptyList
+import io.circe.Codec
+
+case class ValidationError(message: String, errors: NonEmptyList[String]) extends NoStackTrace derives Codec 
