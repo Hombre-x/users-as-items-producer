83d0bbf7fd415e49a954d634b1ca0d0fc64df663
Hombre-x <gabisanpar@gmail.com>
2025-09-23 00:35:16 -0500

    🏷️ Add PostgreSQL configuration to AppConfig

--- src/main/scala/domain/config/ConfigTypes.scala
+++ src/main/scala/domain/config/ConfigTypes.scala
@@ -1,14 +1,17 @@
 package com.mycode
 package domain.config
 
+import ciris.Secret
 import com.comcast.ip4s.Port
 
 case class AppConfig(
     producerConfig: ProducerConfig,
     consumerConfig: ConsumerConfig,
+    postgresConfig: PostgreSQLConfig,
     httpConfig: HttpConfig
 )
 
 case class ProducerConfig(bootstrapServer: String)
 case class ConsumerConfig(bootstrapServer: String, groupId: String)
 case class HttpConfig(host: String, port: Port)
+case class PostgreSQLConfig(host: String, port: Int, user: String, password: Secret[String], database: String)
