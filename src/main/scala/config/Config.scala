c292bcae6a8a6926177688fddf6196c34735866f
Hombre-x <gabisanpar@gmail.com>
2025-09-23 00:36:23 -0500

    🔧 Add PostgreSQL environment variables

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -3,7 +3,7 @@ package config
 
 import cats.syntax.all.*
 import cats.effect.Async
-import domain.config.{AppConfig, ConsumerConfig, HttpConfig, ProducerConfig}
+import domain.config.*
 
 import ciris.*
 import com.comcast.ip4s.Port
@@ -13,9 +13,13 @@ object Config:
   def load[F[_]: Async]: F[AppConfig] =
     (
       env("KAFKA_BOOTSTRAP_SERVER").default("localhost:19092"),
-      env("WAREHOUSE_SERVER_PORT").as[Int].default(9000)
+      env("WAREHOUSE_SERVER_PORT").as[Int].default(9000),
+      env("WAREHOUSE_DATABASE_HOST").default("localhost"),
+      env("WAREHOUSE_DATABASE_PORT").as[Int].default(15432),
+      env("WAREHOUSE_DATABASE_USER").default("postgres"),
+      env("WAREHOUSE_DATABASE_PASSWORD").secret
     )
-      .parMapN((kafkaUri, port) =>
+      .parMapN((kafkaUri, port, dbHost, dbPort, dbUser, dbPassword) =>
         AppConfig(
           producerConfig = ProducerConfig(
             kafkaUri
@@ -23,6 +27,13 @@ object Config:
           consumerConfig = ConsumerConfig(
             kafkaUri,
             "default-group"
+          ),
+          postgresConfig = PostgreSQLConfig(
+            dbHost,
+            dbPort,
+            dbUser,
+            dbPassword,
+            "warehouse-db"
           ),
           httpConfig = HttpConfig(
             "localhost",
