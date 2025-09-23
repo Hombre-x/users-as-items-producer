6e6df8749bc433d3eb81fdad10334bb9e8775595
Hombre-x <gabisanpar@gmail.com>
2025-09-23 00:42:54 -0500

    👔 Update name codec to use ValidName refinement

--- src/main/scala/domain/skunk/UserCodecs.scala
+++ src/main/scala/domain/skunk/UserCodecs.scala
@@ -15,6 +15,6 @@ object UserCodecs:
   val userId: Codec[UserId]     = uuid
   val username: Codec[Username] = varchar.refined[Alphanumeric].imap(Username.apply)(_.value)
   val email: Codec[Email]       = varchar.refined[ValidEmail].imap(Email.apply)(_.value)
-  val name: Codec[Name]         = varchar.refined[ForAll[Letter]].imap(Name.apply)(_.value)
+  val name: Codec[Name]         = varchar.refined[ValidName].imap(Name.apply)(_.value)
 
 end UserCodecs
