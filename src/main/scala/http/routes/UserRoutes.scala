a84793eb7b40c094a20c611fa1ae9d55bd975cdf
Hombre-x <gabisanpar@gmail.com>
2025-09-23 00:49:59 -0500

    🥅 Add error recovery on routes

--- src/main/scala/http/routes/UserRoutes.scala
+++ src/main/scala/http/routes/UserRoutes.scala
@@ -1,57 +1,55 @@
 package com.mycode
 package http.routes
 
-import cats.effect.{Concurrent, MonadCancelThrow, Sync}
+import cats.effect.{Concurrent, MonadCancelThrow}
 import cats.syntax.all.*
-import algebras.Users
 import domain.user.*
+import instances.users.codec.given
 
-import cats.data.Validated
-
-import com.mycode.validation.UserValidation
+import core.Warehouse
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 import org.http4s.circe.CirceEntityCodec.*
 
-class UserRoutes[F[_]: {Sync, Concurrent, MonadCancelThrow}](users: Users[F]) extends Http4sDsl[F]:
+class UserRoutes[F[_]: {Concurrent, MonadCancelThrow}](warehouse: Warehouse[F]) extends Http4sDsl[F]:
 
   private val prefixPath = "/users"
 
   private def queryRoutes: HttpRoutes[F] = HttpRoutes.of[F]:
-    case GET -> Root / username =>
-
-      users
-        .get(Username.applyUnsafe(username))
-        .flatMap:
-          case Some(user) => Ok(user)
-          case None       => NotFound(s"User with username $username not found")
-
-    case GET -> Root / UUIDVar(userId) =>
-      users
-        .getById(userId)
-        .flatMap:
-          case Some(user) => Ok(user)
-          case None       => NotFound(s"User with ID $userId not found")
+    case GET -> Root / username => Ok(s"To be implemented, $username")
 
   private def commandRoutes: HttpRoutes[F] = HttpRoutes.of[F]:
     case req @ POST -> Root =>
       req
         .as[CreateUser]
         .flatMap: createUser =>
-          UserValidation.validateCreateUser(createUser) match
+          warehouse
+            .addUser(createUser)
+            .flatMap: username =>
+              Created(s"User with username $username created")
+            .recoverWith:
+              case UsernameAlreadyExists(username) =>
+                Conflict(s"User with username $username already exists")
 
-            case Validated.Valid(validUser) =>
-              users
-                .create(validUser)
-                .flatMap: username =>
-                  Created(s"User created with username: $username")
+    case req @ PUT -> Root =>
+      req
+        .as[UpdateUser]
+        .flatMap: updateUser =>
+          warehouse
+            .updateUser(updateUser)
+            .flatMap: username =>
+              Ok(s"User with username $username updated")
+            .recoverWith:
+              case UserNotFound(username) =>
+                NotFound(s"User with username $username not found")
 
-            case Validated.Invalid(errors) =>
-              BadRequest(s"Invalid user data: ${errors.toList.mkString(", ")}")
-
-    case _ @PUT -> Root / UUIDVar(userId) => Ok(s"User $userId updated")
-    case DELETE -> Root / UUIDVar(userId) => Ok(s"User $userId deleted")
+    case DELETE -> Root / username =>
+      warehouse
+        .deleteUser(Username.applyUnsafe(username))
+        .flatMap:
+          case true  => Ok(s"User with username $username deleted")
+          case false => NotFound(s"User with username $username not found")
 
   private def httpRoutes: HttpRoutes[F] = queryRoutes <+> commandRoutes
 
