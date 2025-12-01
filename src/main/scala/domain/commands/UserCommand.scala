a36dcda0b2182dc91e7523612bb543ccadbb210d
Hombre-x <gabisanpar@gmail.com>
2025-12-01 17:34:53 -0500

    ✨ Use discriminator in JSON codec for users

--- src/main/scala/domain/commands/UserCommand.scala
+++ src/main/scala/domain/commands/UserCommand.scala
@@ -3,6 +3,10 @@ package domain.commands
 
 import domain.user.*
 
+import io.circe.Codec
+import io.circe.derivation.{Configuration, ConfiguredCodec}
+import io.github.iltotore.iron.circe.given
+
 import java.time.Instant
 
 enum UserCommand:
@@ -13,19 +17,26 @@ enum UserCommand:
   case CreateUserCommand(
       id: CommandId,
       createdAt: Instant,
-      user: CreateUser
+      user: CreateUser,
   )
 
   case DeleteUserCommand(
       id: CommandId,
       createdAt: Instant,
-      username: Username
+      username: Username,
   )
 
   case UpdateUserCommand(
       id: CommandId,
       createdAt: Instant,
-      user: UpdateUser
+      user: UpdateUser,
   )
 
+end UserCommand
+
+object UserCommand:
+  
+  given Configuration = Configuration.default.withDiscriminator("$type")
+  given Codec[UserCommand] = ConfiguredCodec.derived
+  
 end UserCommand
