9e7d89e0d94f2da49088efaede07393d0796679e
Hombre-x <gabisanpar@gmail.com>
2025-12-01 17:35:41 -0500

    🔥 Removed derived scodec instances

--- src/main/scala/instances/users/codec/UserCodecInstances.scala
+++ src/main/scala/instances/users/codec/UserCodecInstances.scala
@@ -1,13 +1,11 @@
 package com.mycode
 package instances.users.codec
 
-import scodec.Codec
 import cats.effect.Concurrent
 
 import org.http4s.{EntityDecoder, EntityEncoder}
 import org.http4s.circe.*
 
-import io.github.iltotore.iron.scodec.given
 
 import domain.user.*
 
@@ -15,6 +13,3 @@ import domain.user.*
 given [F[_]]: EntityEncoder[F, CreateUser]             = jsonEncoderOf[F, CreateUser]
 given [F[_]: Concurrent]: EntityDecoder[F, CreateUser] = accumulatingJsonOf[F, CreateUser]
 
-given Codec[CreateUser] = Codec.derived
-given Codec[UpdateUser] = Codec.derived
-given Codec[User]       = Codec.derived
