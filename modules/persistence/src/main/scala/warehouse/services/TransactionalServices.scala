eafd9ee9b15e7faea70c96bd4cd4afa3167bbc95
Hombre-x <gabisanpar@gmail.com>
2025-12-27 01:02:30 -0500

    ✨ Add transactional services alg

---
+++ modules/persistence/src/main/scala/warehouse/services/TransactionalServices.scala
@@ -0,0 +1,26 @@
+package warehouse.services
+
+import cats.effect.kernel.{Resource, Sync}
+import warehouse.algebras.{Notifier, Outbox, Users}
+import warehouse.domain.skunk.Pool
+
+object TransactionalServices:
+
+  def make[F[_] : Sync](postgres: Pool[F]): Resource[F, TransactionalServices[F]] =
+    postgres.map: se =>
+      val _outbox = Outbox.postgresSession[F](se)
+      val _notifier = Notifier.skunk[F](se)
+      new TransactionalServices[F](
+        users = Users.fromSession[F](_notifier, _outbox, se),
+        outbox = _outbox,
+        notifier = _notifier
+      ) {}
+
+end TransactionalServices
+
+sealed abstract class TransactionalServices[F[_]] private (
+    val users: Users[F],
+    val outbox: Outbox[F],
+    val notifier: Notifier[F]
+)
+
