a4cc353f4b47b5566eb60a940db3a89d5d6efd59
Hombre-x <gabisanpar@gmail.com>
2025-12-26 19:30:24 -0500

    ✨ Add Transactor algebra

---
+++ modules/persistence/src/main/scala/warehouse/algebras/Transactor.scala
@@ -0,0 +1,16 @@
+package warehouse.algebras
+
+import cats.effect.MonadCancelThrow
+import skunk.Session
+import warehouse.domain.skunk.Pool
+
+trait Transactor[F[_]]:
+  def transact[A](f: Session[F] => F[A]): F[A]
+
+object Transactor:
+  def pooledSession[F[_] :{MonadCancelThrow}](postgres: Pool[F]): Transactor[F] =
+    new Transactor[F]:
+      override def transact[A](f: Session[F] => F[A]): F[A] =
+        postgres.use: session =>
+          session.transaction.use: _ =>
+            f(session)
